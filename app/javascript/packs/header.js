fd585316fb40cd6924898f8e4b250387054d365b
Liz Tiller <elizabeth.tiller@gmail.com>
2017-12-02 19:49:56 -0500

    images

---
+++ app/javascript/packs/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Header extends React.Component {
+
+  render () {
+    return <div className='header'>
+      <h1> Liz & Michael</h1>
+    </div>
+  }
+}
+
+export default Header
