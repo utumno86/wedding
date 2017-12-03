40d66b0c760fbf6d996eb511db32675cd804d51a
Liz Tiller <elizabeth.tiller@gmail.com>
2017-12-02 20:13:56 -0500

    cleaned up and ready for you to tweak

---
+++ app/javascript/packs/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+        <div>
+            <h1>Liz and Michael</h1>
+        </div>, document.getElementById('main'))
+})
