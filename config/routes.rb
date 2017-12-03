85d5a4c732013f871c33df0c007373ec05889c03
Liz Tiller <elizabeth.tiller@gmail.com>
2017-12-02 22:36:03 -0500

    fixed route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/', to: 'home#index'
 
-  root to: '/'
+  root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
