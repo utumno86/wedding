0e387dff3ea0dc142275136fdef5d7fbdaadcb41
Michael Nash <michael@spatialnetworks.com>
2018-05-14 16:46:04 -0400

    mirrored server-side routing to client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+
   get '/', to: 'home#index'
+  get 'about', to: 'pages#about'
+  get 'ThingsToDo', to: 'pages#things'
+  get 'PlacesToStay', to: 'pages#places'
 
   root to: 'home#index'
 
