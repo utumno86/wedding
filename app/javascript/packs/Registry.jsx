d1e6a02159ba546940f00a9ab575200096ee2e31
Liz Tiller <elizabeth.tiller@gmail.com>
2018-09-23 21:05:44 -0400

    registry

--- app/javascript/packs/Registry.jsx
+++ app/javascript/packs/Registry.jsx
@@ -33,7 +33,13 @@ class Registry extends Component {
         return (
             <RegistryPage>
                 <Box>
-                    <Tagline>Registry information coming soon</Tagline>
+                    <Tagline>Registry</Tagline>
+                    <p>
+                        As many of you know, Michael inherited the house his grandparents built in the 1950s.  It is a wonderful house that we adore and appreciate, but due to its age, it needs quite a bit of upkeep.  Last year, we redid the bathroom, which had many soft spots.  Three years ago, Michael and his father replaced the roof.  More soft spots mean the floor needs to be replaced throughout the house, the windows needs replacing, and the slowly collapsing garage houses the water pump which is powered by an extension cord from the house.
+                    </p>
+                    <p>
+                        Although our registry is available on <a href="https://www.amazon.com/wedding/share/MikeandLiz">Amazon</a>, we would also appreciate monetary gifts towards our housing fund.  However, any gifts, whether towards the housing fund or from the official registry is appreciated.  
+                    </p>
                 </Box>
             </RegistryPage>
         )
